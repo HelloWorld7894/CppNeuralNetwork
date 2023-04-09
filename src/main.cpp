387c404987463e71ae41f7ca90e26c19e47636d1
HelloWorld7894 <danpojhan@protonmail.com>
2023-04-09 13:33:49 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include "include/json.hpp"
+
+using json = nlohmann::json;
+int main()
+{
+    std::cout << "Hello World";
+    return 0;
+    
+    std::ifstream
+}
